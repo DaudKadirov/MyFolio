cbaba709a9cdea6c3f641aa35bb6d89b5bfa70b4
DaudUzbek2005 <xpk12345670@gmail.com>
2024-09-10 07:27:30 +0500

    Quartz sync: Sep 10, 2024, 7:27 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Inter",
+        header: "Geologica",
         body: "Inter",
         code: "IBM Plex Mono",
       },
